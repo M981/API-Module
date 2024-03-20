72e597db1d72e0ad10b1d1cf506d2dd2885b2ae2
M <114263550+M981@users.noreply.github.com>
2024-03-20 11:23:51 +0100

    feat(app.js): Add POST method

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
+// INIT
 const URL = "https://jsonplaceholder.typicode.com/users";
+const userData = {
+    name: "M H",
+    username: "m981",
+    email: "m981@helpdesk.com",
+}
 
-getUsers()
+// FUNCS
 async function getUsers() {
     await fetch(URL)
     .then(response => response.json())
     .then((data) => console.log(data));
 }
+
+async function createUser(userData) {
+    try {
+        const response = await fetch(URL, { // Wacht op fetch
+            method: "POST", 
+            headers: { 
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(userData)
+        });
+        const data = await response.json(); // API Response -> JSON Structuur
+        console.log(data);
+        server_antwoord_data = data;
+    } catch (err) {
+        console.log("Error posting user:", err);
+    }
+}
+
+getUsers()
+createUser(userData)
