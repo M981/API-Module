2f7e23bc1064ef6823489ed5c8a0ce71a931d3f4
M <114263550+M981@users.noreply.github.com>
2024-03-21 09:55:55 +0100

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,7 +2,15 @@
 
 @include_once('app/APIFunctions.php');
 
-getAllUsers()
+getAllUsers();
+
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    header('Content-Type: application/json');
+    header('HTTP/1.1 200 Ok');
+    echo json_encode($_POST);
+    die();
+ }
 
 ?>
 
